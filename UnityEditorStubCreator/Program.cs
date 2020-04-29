b4da79a31de4564b32a5f44c67f68ff4bf8c84c8
Reinms <reinmasamuneytstuff@gmail.com>
2020-04-29 18:17:31 -0400

    Initial Commit

---
+++ UnityEditorStubCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace UnityEditorStubCreator
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+
+        [MethodImpl( MethodImplOptions.ForwardRef )]
+        public static extern int Square( int number );
+    }
+}
